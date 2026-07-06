bea88f633e99955549e91d84d12209a1a047951a
iamre2024-debug <iamree2024@gmail.com>
2026-07-06 16:57:26 -0500

    Wire tool registry coverage check into app shell

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,6 @@
 import AppWorkstation from "./AppWorkstation.jsx";
+import { assertToolRegistryCoverage } from "./data/toolRegistry.js";
+
+assertToolRegistryCoverage();
 
 export default AppWorkstation;
