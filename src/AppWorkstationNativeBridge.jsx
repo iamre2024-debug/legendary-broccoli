528a8623728c778b72156b407bcc7255e5a30619
iamre2024-debug <iamree2024@gmail.com>
2026-07-08 01:00:52 -0500

    Load lookup report launcher styles

--- src/AppWorkstationNativeBridge.jsx
+++ src/AppWorkstationNativeBridge.jsx
@@ -1,6 +1,7 @@
 import AppWorkstationMatrixBridge from "./AppWorkstationMatrixBridge.jsx";
 import NativePanelRuntime from "./components/NativePanelRuntime.jsx";
 import "./nativePanelRuntime.css";
+import "./lookupReportLauncher.css";
 
 export default function AppWorkstationNativeBridge() {
   return (
