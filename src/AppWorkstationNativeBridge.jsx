acd0d29dc5aeebe7c8b11b79b673757c66b555af
iamre2024-debug <iamree2024@gmail.com>
2026-07-08 02:57:56 -0500

    Load saved Report Center styles

--- src/AppWorkstationNativeBridge.jsx
+++ src/AppWorkstationNativeBridge.jsx
@@ -2,6 +2,7 @@ import AppWorkstationMatrixBridge from "./AppWorkstationMatrixBridge.jsx";
 import NativePanelRuntime from "./components/NativePanelRuntime.jsx";
 import "./nativePanelRuntime.css";
 import "./lookupReportLauncher.css";
+import "./reportCenter.css";
 
 export default function AppWorkstationNativeBridge() {
   return (
