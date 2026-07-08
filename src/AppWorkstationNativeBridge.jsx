74a0d4924bb6977f5d7bfa10a7aa0d49ad622555
iamre2024-debug <iamree2024@gmail.com>
2026-07-08 03:26:34 -0500

    Wire native matrix generate runtime

--- src/AppWorkstationNativeBridge.jsx
+++ src/AppWorkstationNativeBridge.jsx
@@ -1,4 +1,5 @@
 import AppWorkstationV3 from "./AppWorkstationV3.jsx";
+import MatrixGenerateRuntime from "./components/MatrixGenerateRuntime.jsx";
 import NativePanelRuntime from "./components/NativePanelRuntime.jsx";
 import "./nativePanelRuntime.css";
 import "./lookupReportLauncher.css";
@@ -8,6 +9,7 @@ export default function AppWorkstationNativeBridge() {
   return (
     <>
       <AppWorkstationV3 />
+      <MatrixGenerateRuntime />
       <NativePanelRuntime />
     </>
   );
