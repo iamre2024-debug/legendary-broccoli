bf4bf656134e1cec2edf0864bfc7bfd2f4939322
iamre2024-debug <iamree2024@gmail.com>
2026-07-08 03:34:48 -0500

    Mount Customer 360 directory runtime

--- src/AppWorkstationNativeBridge.jsx
+++ src/AppWorkstationNativeBridge.jsx
@@ -1,9 +1,11 @@
 import AppWorkstationV3 from "./AppWorkstationV3.jsx";
 import MatrixGenerateRuntime from "./components/MatrixGenerateRuntime.jsx";
 import NativePanelRuntime from "./components/NativePanelRuntime.jsx";
+import Customer360DirectoryRuntime from "./components/Customer360DirectoryRuntime.jsx";
 import "./nativePanelRuntime.css";
 import "./lookupReportLauncher.css";
 import "./reportCenter.css";
+import "./customer360Directory.css";
 
 export default function AppWorkstationNativeBridge() {
   return (
@@ -11,6 +13,7 @@ export default function AppWorkstationNativeBridge() {
       <AppWorkstationV3 />
       <MatrixGenerateRuntime />
       <NativePanelRuntime />
+      <Customer360DirectoryRuntime />
     </>
   );
 }
